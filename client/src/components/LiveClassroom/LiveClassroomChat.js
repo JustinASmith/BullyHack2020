9f44ed9cc7b693f7a9d54f00db83773a334d3e11
JustinASmith <justinasmith662@gmail.com>
2020-01-12 12:43:10 -0600

    CRAP

--- client/src/components/LiveClassroom/LiveClassroomChat.js
+++ client/src/components/LiveClassroom/LiveClassroomChat.js
@@ -1,14 +1,57 @@
 import React from 'react'
+import io from "socket.io-client";
 
 import { Form, Row, Col, Button, Modal, Container, Card } from 'react-bootstrap'
+const socket = io.connect('http://localhost:3001');
+
+socket.on('chat message', (msg) => {
+    console.log(msg)
+})
+
+const connectToClass = (room) => {
+    socket.emit('connectToClass', room);
+}
+
+const chatMessage = (msg) => {
+    socket.emit('chat message', msg);
+}
+
+const renderMessages = (arr) => {
+    const data = arr.map(obj => {
+       return <p className="m-2">test</p>
+    })
+    return data;
+}
 
 export default function LiveClassroomChat(props) {
+    const [messages, setMessages] = React.useState([]);
+    const [message, setMessage] = React.useState('');
+
     return (
         <Container>
             <Row>
                 <h1 className="mx-auto">
-                    Live Chat for "example classroom title"
+                    Introduction to CSE Classroom
                 </h1>
+            </Row>
+            <Row className="mx-auto m-2" style={{backgroundColor: "#efefef", height: "40vh", width: "75%", border: '1px solid #ddd'}}>
+                <Col className="p-2">
+                    <div className="text-right">
+                        <p className="m-3">Hey, how do you use that variable within that function?</p>
+                        <p className="m-3">What is the purpose of declaring as an int instead of double?</p>
+                        <p className="m-3">Do we need to include that header?</p>
+                    </div>
+                </Col>
+            </Row>
+            <Row className="mx-auto m-2" style={{width: "75%"}}>
+                <Col sm="12" className="mx-auto">
+                    <Form.Control placeholder="Chat here..." />
+                </Col>
+                <Col sm="12">
+                    <Button className="ml-auto mt-2" variant="primary" type="submit" onClick={() => chatMessage(message)}>
+                        Send
+                    </Button>
+                </Col>
             </Row>
         </Container>
     )
