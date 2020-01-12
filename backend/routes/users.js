8682fabd7806c53a968178d8f3204b6f47da4ed3
JustinASmith <justinasmith662@gmail.com>
2020-01-12 11:44:46 -0600

    Temp login route
    NOT FUNCTIONAL

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const Bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 let User = require('../models/user.model');
 
@@ -19,5 +21,55 @@ router.route('/add').post((req, res) => {
         .then(() => res.json('User added!'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
+
+function isCorrectPassword(password, callback) {
+    bcrypt.compare(password, this.password, function(err, same) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(err, same);
+        }
+    });
+}
+
+router.route('/login').post(function(req, res) {
+    const { email, password } = req.body;
+    User.findOne({ email }, function(err, user) {
+      if (err) {
+        res.status(500)
+          .json({
+          error: 'Internal error please try again'
+        });
+      } else if (!user) {
+        res.status(401)
+          .json({
+            error: 'Incorrect email or password'
+          });
+      } else {
+        isCorrectPassword(password, function(err, same) {
+            console.log('test')
+          if (err) {
+            res.status(500)
+              .json({
+                error: 'Internal error please try again'
+            });
+          } else if (!same) {
+            res.status(401)
+              .json({
+                error: 'Incorrect email or password'
+            });
+          } else {
+            // Issue token
+            const payload = { email };
+            const token = jwt.sign(payload, secret, {
+              expiresIn: '2h'
+            });
+            res.cookie('token', token, { httpOnly: true })
+              .sendStatus(200);
+          }
+        });
+      }
+    });
+  });
 
 module.exports = router;
